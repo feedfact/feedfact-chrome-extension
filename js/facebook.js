03d41438ff3fcf1298c6c42699493ad963bc35a7
Tony Doran <tdoran@atlassian.com>
2017-02-19 14:26:02 -0800

    cleaned up the style of the overlay

--- js/facebook.js
+++ js/facebook.js
@@ -1,3 +1,62 @@
+//function used to inject styles into the dom
+var style_injector = function(s) {
+
+  var style = ""
+  Object.keys(s).forEach(function(k) {
+    style += k+" {"
+    Object.keys(s[k]).forEach(function(k2) {
+      style += k2+":"+s[k][k2]+";"
+    });
+    style += "} ";
+
+  });
+
+  var style_el = document.createElement("style");
+  style_el.innerHTML = style;
+  document.body.appendChild(style_el);
+}
+
+style_injector({
+  ".ff-title": {
+    "position" : "absolute",
+    "top" : "-255px",
+    "z-index" : "999",
+    "padding" : "5px",
+    "font-size" : "12px",
+    "line-height" : "1.5",
+    "background-color" : "rgba(0,0,0,0.8)",
+    "color" : "#fff",
+    "border-radius" : "5px",
+    "width" : "100px",
+    "left" : "-7px"
+  },
+  ".ff-title a" : {
+    "color" :"white",
+    "text-decoration" : "underline",
+    "font-weight" : "500",
+  },
+  ".ff-title a:hover" : {
+    "color" :"#ff2e88",
+  },
+  ".ff-row" : {
+    "margin-bottom" : "2px",
+    "display" : "block",
+    "background-color" : "#ff2e88",
+    "text-align" : "right",
+    "font-weight" : "300"
+  },
+  ".ff-row span" : {
+    "position" : "absolute",
+    "left" : "0px",
+    "padding-left" : "7px"
+  }
+});
+
+
+
+
+
+
 var ffQueue = [];
 
 setInterval(function() {
@@ -26,16 +85,14 @@ var request = new XMLHttpRequest();
       if (request.readyState === 4) {
           if (request.status === 200) {
               var data = JSON.parse(request.responseText);
-              console.log(data);
-              //var clickbait = data.clickbaitiness;
               if  (data && !data.Item) {
-                let html = "<ul style='position:absolute;top:-246px;z-index:999;padding:5px;font-size:12px;line-height:1.8;background-color:rgba(0, 0, 0, 0.73);color:#fff;border-radius:5px'>No ratings yet on Feedfact.org</ul>";
+                let html = "<ul class=\"ff-title\">No ratings yet on Feedfact.org</ul>";
                 el.insertAdjacentHTML('afterend', html);
               } else {
-                let html = "<ul style='position:absolute;top:-246px;z-index:999;padding:5px;font-size:12px;line-height:1.8;background-color:#000;color:#fff;border-radius:5px'>Feedfact.org ranked this "+data.Item.rankings.cited.samples+" times";
+                let html = "<ul class=\"ff-title\"><a href=\"https://feedfact.org/\">Feedfact.org</a>";
 
                 Object.keys(data.Item.rankings).forEach(function(a) {
-                  html += "<div style=\"margin-bottom:2px;display:block;background-color:#ff2e88;text-align:right;width:"+(data.Item.rankings[a].avg*100)+"%\">&nbsp;<span style=\"position:absolute;left:0px;padding-left:7px;\">"+(Math.round(data.Item.rankings[a].avg * 100)) +"% "+a+"</span></div>"
+                  html += "<div class=\"ff-row\" style=\"width:"+(data.Item.rankings[a].avg*100)+"%\">&nbsp;<span>"+(Math.round(data.Item.rankings[a].avg * 100)) +"% "+a+"</span></div>"
                 });
                 el.insertAdjacentHTML('afterend', html);
               }
